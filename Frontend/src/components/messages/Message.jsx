5a50b0a636389c3c842201edec33acf87e953256
SwaroopDangal <swaroopdangal732@gmail.com>
2025-06-10 22:15:39 +0545

    ui completd

---
+++ Frontend/src/components/messages/Message.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/036/594/092/non_2x/man-empty-avatar-photo-placeholder-for-social-networks-resumes-forums-and-dating-sites-male-and-female-no-photo-images-for-unfilled-user-profile-free-vector.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white bg-blue-500`}>Hi whats up</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        12:42
+      </div>
+    </div>
+  );
+};
+
+export default Message;
