674c69855fa28206301ee514d51beacdc61c8aca
SwaroopDangal <swaroopdangal732@gmail.com>
2025-06-16 14:32:52 +0545

    home fixed

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
-
 import Sidebar from "../../components/Sidebar/Sidebar";
-import MessageContainer from "../../components/messages/MessageCOntainer";
+import MessageContainer from "../../components/messages/MessageContainer";
 
 const Home = () => {
   return (
