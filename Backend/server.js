edc6ac914d3d2ac53990416553272adbc49354ea
SwaroopDangal <swaroopdangal732@gmail.com>
2025-06-16 14:50:32 +0545

    F

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/users", userRoute);
 
 app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
