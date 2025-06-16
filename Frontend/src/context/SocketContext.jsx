cf9547eea17b60268aa95f1a1adce3e78d1a7e5d
SwaroopDangal <swaroopdangal732@gmail.com>
2025-06-16 14:59:35 +0545

    url update

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (!authUser) return;
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://chatify-vn65.onrender.com/", {
         query: {
           userId: authUser._id,
         },
